818ebc4da0940addc16bd1587f83df74e21295ae
Joonas Villanen <joonas.villa@gmail.com>
2021-11-16 18:00:23 +0100

    Page: Index

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -6,27 +6,4 @@ class ExperiencesController < ApplicationController
     @experiences = Experience.all
   end
 
-  def show; end
-
-  def new
-    @experience = Experience.new
-  end
-
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
-
-  private
-
-  def set_experience
-    @experience = Experience.find(params[:id])
-  end
 end
