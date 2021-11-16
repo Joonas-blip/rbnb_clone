c6655f49b1f75e0a4d2d653e51af424550f85bad
Joonas Villanen <joonas.villa@gmail.com>
2021-11-16 12:16:52 +0100

    SETUP DONE

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
+  STATUS = %w(pending confirmed rejected)
   belongs_to :user, :experience
   validates :start_time, :end_time, presence: true
+  validates :status, inclusion: { in: STATUS }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,23 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  ROLE = %w(user guide)
   has_many :bookings, dependent: :destroy
   has_many :experiences, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :username, presence: true, length: { minimum: 8 }
-  validates :email, presence: true, uniqueness: true, format: { with: /\w+@\w+/ }
-  validates :encrypted_password, presence: true, length: { minimum: 8 }
-  validates :role, inclusion: { in: %w(user guide) }, presence: true
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
+  validates :role, inclusion: { in: ROLE }, presence: true
   validates :first_name, :last_name, presence: true
 
   # INSTANCE METHODS
+  def user?
+    self.role == 'user'
+  end
+
+  def guide?
+    self.role == 'guide'
+  end
 end
