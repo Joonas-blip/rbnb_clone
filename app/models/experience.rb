76f28e24484a42889136e4eb300fb26acfa987d8
Joonas Villanen <joonas.villa@gmail.com>
2021-11-17 17:28:09 +0100

    Fix: temp commnet

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,8 +2,8 @@ class Experience < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
   validates :title, :price, :address, :duration, presence: true
   validates :description, presence: true, length: { minimum: 70 }
 end
