8bb00904ffce4f80d97ca2a8b9009ed9178eb577
Joonas Villanen <joonas.villa@gmail.com>
2021-11-17 16:14:49 +0100

    fix

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,12 +2,8 @@ class Experience < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
-<<<<<<< HEAD
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   validates :title, :price, :address, :duration, presence: true
-=======
-  validates :title, :price, :location, :duration, presence: true
->>>>>>> f4cb4d454dabfd6220e79061332e4d956b187b6d
   validates :description, presence: true, length: { minimum: 70 }
 end
