05a1f36829d88745888ea888608256a695c327f8
Joonas Villanen <joonas.villa@gmail.com>
2021-11-17 15:31:31 +0100

    Map and index

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,8 @@ User.create(email: 'user2@mail.com', username: 'user2', password: '123456', role
   file = URI.open('https://picsum.photos/200/200')
   experience = Experience.new(
     title: Faker::Verb.base,
-    location: Faker::Address.city,
+    address: "Ciutat de balaguer, 25, Barcelona",
+    # Faker::Address.city,
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     price: rand(1..1500),
     duration: rand(1..24),
