1af4e2f8bc550f5a9e7c1dba42495678975bf8e5
Naima Duyser <naima.fatimaduyser@gmail.com>
2021-11-17 16:41:16 +0100

    scheme update

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2021_11_17_140355) do
-=======
-ActiveRecord::Schema.define(version: 2021_11_16_154212) do
->>>>>>> f4cb4d454dabfd6220e79061332e4d956b187b6d
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,10 +59,7 @@ ActiveRecord::Schema.define(version: 2021_11_16_154212) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.boolean "availability", default: true
-    t.float "latitude"
-    t.float "longitude"
     t.string "address"
-
     t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
