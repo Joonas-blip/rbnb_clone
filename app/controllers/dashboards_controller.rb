eedf4c5d1ea76e4a027d0dd7a14d259db321106c
Andrea Corbetta <corbettaandrea.dev@gmail.com>
2021-11-19 12:46:16 +0100

    implemented different UXs for users

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,5 @@
 class DashboardsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :validate_user
 
   def profile
       @dash_experiences = Experience.where(user:current_user)
@@ -7,5 +7,13 @@ class DashboardsController < ApplicationController
 
   def bookings
     @dash_bookings = Booking.where(user:current_user)
+  end
+
+  private
+
+  def validate_user
+    if current_user.user?
+      redirect_to '/'
+    end
   end
 end
